eff1ac44ca149048fdaae3060a12bc3b0c88be73
Ubuntu <ubuntu@ip-172-31-10-135.eu-west-1.compute.internal>
2017-04-06 11:51:44 +0000

    Started running the galaxies dataset on GPU. The model is not converging yet. I suspect the softmax layer and loss function.

--- deeplearning1/nbs/imagedataset.py
+++ deeplearning1/nbs/imagedataset.py
@@ -6,9 +6,12 @@ import numpy as np
 import torch
 
 def open_image(path):
-    return Image.open(path).convert('RGB')
+    return Image.open(path).convert('RGB').resize((224, 224), Image.BILINEAR)
 
-def get_dataframe_row_by_id(df, id):
+def get_dataframe_row_by_id(df, id, test_mode):
+    if test_mode:
+        # TODO can we do better than this?
+        return 0
     # Return a DataFrame row by its id in numpy format
     row = df.loc[id].as_matrix().astype(np.float32)
     return torch.FloatTensor(row)
@@ -20,21 +23,18 @@ def get_image_id(file_path):
     file_id = filename.rsplit('.',1)[0]
     return int(file_id)
 
-def make_dataset(dir, target_dataframes):
+def make_dataset(dir, target_dataframes, test_mode):
     # Read all image files in directory
     images_paths = glob.glob(dir +"/*.jpg")
     
-    # convert them to tensors
-    transform = transforms.Compose([
-        transforms.Lambda(lambda img: img.resize((224, 224), Image.BILINEAR)),
-        transforms.ToTensor(),
-        transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
-    ])
+    # TODO
+    if test_mode:
+        print("Using test mode")
     
     # Create an array of tuples (tensor image and its target vector), one row per image
     images_targets = [ 
-        (transform(open_image(image_path)), # image
-         get_dataframe_row_by_id(target_dataframes, get_image_id(image_path))) # target
+        (open_image(image_path), # image
+         get_dataframe_row_by_id(target_dataframes, get_image_id(image_path), test_mode)) # target
         for image_path in images_paths]
         
     return images_targets, images_paths
@@ -44,10 +44,15 @@ def get_images_ids_from_image_paths(images_paths):
 
 class ImageDataset(data.Dataset):
 
-    def __init__(self, root, target_dataframes):
-        images_targets, images_paths = make_dataset(root, target_dataframes)
+    def __init__(self, root, target_dataframes, test_mode):
+        images_targets, images_paths = make_dataset(root, target_dataframes, test_mode)
         if len(images_targets) == 0:
             raise(RuntimeError("Found 0 images in subfolders of: " + root ))
+        
+        self.image_transform = transforms.Compose([
+            transforms.ToTensor(),
+            transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
+        ])
 
         self.root = root
         self.images_targets = images_targets
@@ -56,7 +61,8 @@ class ImageDataset(data.Dataset):
 
     def __getitem__(self, index):
         image, target = self.images_targets[index]
-        return image, target
+        tensor = self.image_transform(image)
+        return tensor, target
 
     def __len__(self):
         return len(self.images_targets)
